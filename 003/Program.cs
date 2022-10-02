041b7b7216b346042c01bbb8d3be817d0eda87b3
tema <kypidon71@gmail.com>
2022-10-02 11:10:39 +0300

    ispravil 003

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,22 +1,23 @@
 ﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]*/
-int[] mass = new int[8];
-        mass[0] = new Random().Next(0, 10);
-        Console.Write(mass[0]);
-            mass[1] = new Random().Next(0, 10);
-             Console.Write(mass[1]);
-             mass[2] = new Random().Next(0, 10);
-                 Console.Write(mass[2]);
-                 mass[3] = new Random().Next(0, 10);
-                     Console.Write(mass[3]);
-                     mass[4] = new Random().Next(0, 10);
-                         Console.Write(mass[4]);
-                            mass[5] = new Random().Next(0, 10);
-                             Console.Write(mass[5]);
-                                mass[6] = new Random().Next(0, 10);
-                                 Console.Write(mass[6]);
-                                    mass[7] = new Random().Next(0, 10);
-                                     Console.Write(mass[7]);
-                                    
-    
+
+void PrintArray (int[] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+            Console.Write($"{matrix[i]} ");
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+                matrix[i] = new Random().Next(0,10);
+        }
+    }
+int[] matrix = new int[8];
+FillArray (matrix);
+PrintArray(matrix);
