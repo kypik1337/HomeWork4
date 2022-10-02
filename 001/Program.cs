2c89d7cab2ebaae91809b7ae67551a07fdf658bc
tema <kypidon71@gmail.com>
2022-10-02 11:28:30 +0300

    peredelalVtoroiRaz001

--- 001/Program.cs
+++ 001/Program.cs
@@ -7,13 +7,24 @@ Console.WriteLine ("add number B");
 int B = Convert.ToInt32(Console.ReadLine()!);
 //Console.WriteLine($"A^B= " + Math.Pow(A,B));
 
-int metod (int A, int B)
+/*int metod (int A, int B)
     {
      int i = Convert.ToInt32(Math.Pow(A,B));  
      return i;    
     }
     int Stepen = metod (A, B);
-Console.WriteLine($"значение {A} в степени {B} = {Stepen}");
+Console.WriteLine($"значение {A} в степени {B} = {Stepen}");*/
 
 
-
+int metod (int A, int B)
+{
+  int result = 1;
+  for(int i=1; i <= B; i++)
+  {
+    result = result * A;
+   
+  }
+  return result;
+}
+ int Stepen = metod(A, B);
+Console.WriteLine(Stepen);
